8d466288b9bac12c97a6a47900821116e1e109e0
mahesh maurya <mahesh@wohlig.com>
2016-10-14 10:45:09 +0530

    project setup

--- www/js/app.js
+++ www/js/app.js
@@ -41,13 +41,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+
     })
     .state('app.playlists', {
       url: '/playlists',
@@ -69,5 +67,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/login');
 });
